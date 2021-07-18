c1dc9eed6accb9bb35cfa26f0bcbf4985ad6205b
João Salomão <joaosalomao.613@gmail.com>
2021-07-18 18:42:11 -0300

    Adicionando controle

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { Map, View } from 'ol';
 import dataLayers from './layers/data'
 import tileLayer from './layers/tile';
+import LayersControl from './controls/LayersControl'
+import { defaults as defaultControls } from 'ol/control';
 
 new Map({
-  target: 'map',
+  controls: defaultControls().extend([new LayersControl(dataLayers)]),
   layers: [
     tileLayer,
     ...dataLayers
   ],
+  target: 'map',
   view: new View({
     center: [0, 0],
     zoom: 2
